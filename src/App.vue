c3a9016315c3a9886f895b595c359e6a60c602f0
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-07-21 14:05:26 +0200

    Checkbox Types done

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,41 @@ export default {
   data() {
     return {
       restaurants: [],
-      store
+      store,
+      types: null,
+      selectedTypes: []
     }
   },
+  
   mounted() {
     this.getRestaurants();
+    this.getTypes();
+  },
+  watch:{
+      selectedTypes:{
+        handler:"getRestaurants",
+        deep:true
+      }
   },
   methods: {
     getRestaurants() {
-      axios.get(`${this.store.baseUrl}/restaurants`)
+
+      const params = {}
+
+      if(this.selectedTypes.length > 0){
+        params.type_id = this.selectedTypes.join(',')
+      }
+
+      axios.get(`${this.store.baseUrl}/restaurants`, { params })
         .then(res => {
           this.restaurants = res.data.restaurants
         })
+    },
+    getTypes(){
+      axios.get(`${this.store.baseUrl}/types`)
+        .then(res => {
+          this.types = res.data.types
+      })
     }
   }
 }
@@ -32,11 +55,27 @@ export default {
 <template>
   <headerComp />
 
-  <ul>
-    <li v-for="(el, index) in restaurants" :key="index">
-      {{ el.name }}
-    </li>
-  </ul>
+  <!-- checkbox Types  -->
+  <div class="container my-3">
+      <button type="button" class="fs-6 btn bg-success form-select form-select-sm" data-bs-toggle="collapse" data-bs-target="#collapseTypes" role="button" aria-expanded="false" aria-controls="collapseTypes">
+        <span>Types</span>
+      </button>
+      <div id="collapseTypes" class="collapse mt-2 p-4 rounded">
+        <div class="rounded">
+          <label v-for="(el,index) in types" :key="index" :for="el.name" class="me-2">
+            <input class="me-1" type="checkbox" :value="el.id" v-model="selectedTypes" :id="el.name">{{ el.name }}
+          </label>
+        </div>
+      </div>
+      
+      <!-- lista ristoranti -->
+      <ul class="mt-4">
+        <li v-for="(el, index) in restaurants" :key="index">
+          {{ el.name }}
+        </li>
+      </ul>
+    </div>
+
 
   <!-- <router-view></router-view> -->
 </template>
