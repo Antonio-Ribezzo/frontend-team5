e65ae658b35abec2826c155f3db3f0256cb227b6
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-07-21 11:44:40 +0200

    navbar fixed

--- src/components/headerComp.vue
+++ src/components/headerComp.vue
@@ -6,47 +6,16 @@
 
 <template>
 
-    <!-- <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container d-flex justify-content-between">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Register your Restaurant</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Login</a>
-                            </li>
-                        </ul>                    
-                    </div>
-
-                    <div>
-                        <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </div> -->
-
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container d-flex justify-content-between">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand me-auto" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="d-flex flex-column flex-lg-row">
-                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="collapse navbar-collapse me-auto" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -60,7 +29,7 @@
                         </ul>
                     </div>
 
-                    <div class="ms-lg-3 mt-3 mt-lg-0">
+                    <div class="ms-lg-3 align-self-center ms-3">
                         <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
                     </div>
                 </div>
