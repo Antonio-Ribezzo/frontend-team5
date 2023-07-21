8e45bf71bf9fe201c7ba3c7f3ba835b966bd0774
RiccardoMartella <riccardomartella77@gmail.com>
2023-07-21 10:02:41 +0200

    data return (base Url)

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,47 @@
 <script>
-  import axios from 'axios';
-  import headerComp from './components/headerComp.vue';
-  import {store} from './store';
+import axios from 'axios';
+import headerComp from './components/headerComp.vue';
+import { store } from './store';
 
-  export default{
-    name: 'App',
-    components:{
-            headerComp
-        },
-    data(){
-      return{
-        restaurants:[],
-        baseUrl:'',
-        store
-      }
-    },
-    mounted(){
-      this.getRestaurants();
-    },
-    methods: {
-      getRestaurants(){
-        axios.get(`${this.store.baseUrl}/restaurants`)
-        .then(res=>{
+export default {
+  name: 'App',
+  components: {
+    headerComp
+  },
+  data() {
+    return {
+      restaurants: [],
+      store
+    }
+  },
+  mounted() {
+    this.getRestaurants();
+  },
+  methods: {
+    getRestaurants() {
+      axios.get(`${this.store.baseUrl}/restaurants`)
+        .then(res => {
           this.restaurants = res.data.restaurants
         })
-      }
     }
   }
+}
 
 </script>
 
 <template>
-  <headerComp/>
+  <headerComp />
 
   <ul>
-    <li v-for="(el,index) in restaurants" :key="index">
+    <li v-for="(el, index) in restaurants" :key="index">
       {{ el.name }}
     </li>
   </ul>
 
   <!-- <router-view></router-view> -->
-  
 </template>
 
 <style lang="scss">
-  // importo il foglio di stile principale
-  @use './style/main.scss' as *;
-
+// importo il foglio di stile principale
+@use './style/main.scss' as *;
 </style>
