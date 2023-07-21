388bd417484865294e8d9ad436295389da9b9337
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-07-21 09:54:55 +0200

    First Api call

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <script>
+  import axios from 'axios';
   import headerComp from './components/headerComp.vue';
+  import {store} from './store';
 
   export default{
     name: 'App',
     components:{
             headerComp
-        }
+        },
+    data(){
+      return{
+        restaurants:[],
+        baseUrl:'',
+        store
+      }
+    },
+    mounted(){
+      this.getRestaurants();
+    },
+    methods: {
+      getRestaurants(){
+        axios.get(`${this.store.baseUrl}/restaurants`)
+        .then(res=>{
+          this.restaurants = res.data.restaurants
+        })
+      }
+    }
   }
 
 </script>
 
 <template>
   <headerComp/>
+
+  <ul>
+    <li v-for="(el,index) in restaurants" :key="index">
+      {{ el.name }}
+    </li>
+  </ul>
 
   <!-- <router-view></router-view> -->
   
